0c43c24535c50f6bb37fd18c269a3202c825d928
engoSuopu <qq073151815733397@163.com>
2021-04-16 14:57:02 +0800

    结论是： 三条线程跑同一个资源，每一条都跑十次。

--- src/main/java/com/multithread/RunnableAdvantage.java
+++ src/main/java/com/multithread/RunnableAdvantage.java
@@ -3,16 +3,16 @@ package com.multithread;
 /**
  * 描述:
  * ----实现runnable接口的优势。可以多个相同代码去处理同一个资源。
+ *      结论-----  这个情况下，起三条线程，每一条都跑了十次。
  *
  * @author ma.xiaofeng
  * @create 2018-05-25 14:31
  */
 public class RunnableAdvantage implements Runnable{
 
-    int i = 0;
 
     public void run() {
-
+        int i = 0;
         while (i<10) {
             System.out.println("线程" + Thread.currentThread().getName() + "数了第" + (i++) + "个数字。");
         }
